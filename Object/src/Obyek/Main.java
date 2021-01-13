0c0385d912e19042dd8e48b5b61b2000f891248e
Dianminii <74170971+Dianminii@users.noreply.github.com>
2021-01-13 09:45:07 +0700

    Add files via upload

---
+++ Object/src/Obyek/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Obyek;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Main {
+  public static void main(String[] args) {
+        Hewan Elang = new Hewan("Elang");
+        Hewan Kucing = new Hewan("Kucing");
+        Elang.beratHewan(5);
+        Elang.jumlahKakiHewan(2);
+        Elang.cetakHewan();
+        Kucing.beratHewan(2);
+        Kucing.jumlahKakiHewan(4);
+        Kucing.cetakHewan();
+    }  
+}
